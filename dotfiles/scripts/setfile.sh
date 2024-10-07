4850d078cd666b8654a3673e818aaa0ade68bf07
Takuya-Mukai <takuya.mukai@gmail.com>
2024-10-07 18:56:00 +0900

    2024/10/07

--- dotfiles/scripts/setfile.sh
+++ dotfiles/scripts/setfile.sh
@@ -2,3 +2,6 @@ sudo cp ~/.config/dotfiles/files/keyball.rules /etc/udev/rules.d/keyball44.rules
 sudo cp ~/.config/dotfiles/files/environment /etc/environment
 sudo cp ~/.config/dotfiles/files/.bashrc ~/.bashrc
 sudo cp ~/.config/dotfiles/files/.bash_profile ~/.bash_profile
+sudo cp ~/.config/dotfiles/files/environments /etc/greetd/environments
+sudo cp ~/.config/dotfiles/files/sway-config /etc/greetd/sway-config
+sudo cp ~/.config/dotfiles/files/config.toml /etc/greetd/config.toml
--- dotfiles/scripts/systemd.sh
+++ dotfiles/scripts/systemd.sh
@@ -6,3 +6,4 @@ sudo systemctl enable tailscale.service
 echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
 echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p /etc/sysctl.conf
+sudo systemctl enable greetd.service
