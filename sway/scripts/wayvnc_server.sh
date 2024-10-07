b270c6fff4f45265329966b50aa5987c13daca5a
Takuya-Mukai <takuya.mukai@gmail.com>
2024-10-07 22:47:56 +0900

    2024/10/07 2

--- sway/scripts/wayvnc_server.sh
+++ sway/scripts/wayvnc_server.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # ホスト名を取得
-hostname=$(hostname)
+hostname=$(cat /etc/hostname)
 
 # ホスト名が archdesk の場合
-if [ "$hostname" == "archdesk" ]; then
-    wayvnc port 5900 &
-    wayvnc port 5901 &
+if [ "$hostname" = "archdesk" ]; then
+  wayvnc -S /tmp/wayvnc_default1.sock -o=DP-1 127.0.0.1 5900 &
+  wayvnc -S /tmp/wayvnc_default2.sock -o=HDMI-A-1 127.0.0.1 5901 &
 fi
-
