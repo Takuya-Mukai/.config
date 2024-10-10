6368eae5580436197cb97c25e2b994793ed0f813
Takuya-Mukai <takuya.mukai@gmail.com>
2024-10-10 14:49:55 +0900

    2024/10/10 caps to ctrl

--- sway/scripts/caps2esc.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/bash
-hostname=$(cat /etc/hostname)
-
-if [ "$hostname" = "archmini" ]; then
-caps2sc -m 0 &
-fi
--- sway/scripts/wayvnc_server.sh
+++ sway/scripts/wayvnc_server.sh
@@ -5,6 +5,6 @@ hostname=$(cat /etc/hostname)
 
 # ホスト名が archdesk の場合
 if [ "$hostname" = "archdesk" ]; then
-  wayvnc -S /tmp/wayvnc_default1.sock -o=DP-1 127.0.0.1 5900 &
-  wayvnc -S /tmp/wayvnc_default2.sock -o=HDMI-A-1 127.0.0.1 5901 &
+  wayvnc -S /tmp/wayvnc_default1.sock -o=DP-1 127.0.0.1 5900
+  wayvnc -S /tmp/wayvnc_default2.sock -o=HDMI-A-1 127.0.0.1 5901
 fi
