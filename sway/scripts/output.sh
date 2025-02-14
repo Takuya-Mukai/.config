d136ef16daf1bb7fcf24b5ba865faf2ed098650f
Takuya-Mukai <takuya.mukai@gmail.com>
2025-02-14 13:18:08 +0900

    2025/02/14 workspace for archdesk v0.0

--- sway/scripts/output.sh
+++ sway/scripts/output.sh
@@ -6,25 +6,35 @@ if [ "$HOSTNAME" = "archdesk" ]; then
   swaymsg output DP-1 resolution 1920x1080 position 0 0 transform 270
   swaymsg output HDMI-A-1 resolution 1920x1080 position 1080 420 
   swaymsg output DP-2 resolution 1920x1080 position 1080 1500
-  swaymsg workspace 1 output DP-1
-  swaymsg workspace 2 output DP-1
-  swaymsg workspace 3 output DP-1
-  swaymsg workspace 4 output DP-1
-  swaymsg workspace 5 output DP-2
-  swaymsg workspace 6 output HDMI-A-1
-  swaymsg workspace 7 output HDMI-A-1
-  swaymsg workspace 8 output HDMI-A-1
-  swaymsg workspace 9 output HDMI-A-1
-  swaymsg workspace 10 output DP-2
+  swaymsg workspace 2
+  swaymsg move workspace to output DP-1
+  swaymsg workspace 3
+  swaymsg move workspace to output DP-1
+  swaymsg workspace 4
+  swaymsg move workspace to output DP-1
+  swaymsg workspace 6
+  swaymsg move workspace to output HDMI-A-1
+  swaymsg workspace 7
+  swaymsg move workspace to output HDMI-A-1
+  swaymsg workspace 8
+  swaymsg move workspace to output HDMI-A-1
+  swaymsg workspace 9
+  swaymsg move workspace to output HDMI-A-1
+  swaymsg workspace 10
+  swaymsg move workspace to output DP-2
+  swaymsg workspace 5
+  swaymsg move workspace to output DP-2
+  swaymsg workspace 1
+  swaymsg move workspace to output DP-1
 elif [ "$HOSTNAME" = "archmini" ]; then
-  swaymsg workspace 1 output DP-2
-  swaymsg workspace 2 output DP-2
-  swaymsg workspace 3 output DP-2
-  swaymsg workspace 4 output DP-2
-  swaymsg workspace 5 output DP-2
-  swaymsg workspace 6 output HDMI-A-2
-  swaymsg workspace 7 output HDMI-A-2
-  swaymsg workspace 8 output HDMI-A-2
-  swaymsg workspace 9 output HDMI-A-2
-  swaymsg workspace 10 output HDMI-A-2
+  swaymsg move workspace 1 output DP-2
+  swaymsg move workspace 2 output DP-2
+  swaymsg move workspace 3 output DP-2
+  swaymsg move workspace 4 output DP-2
+  swaymsg move workspace 5 output DP-2
+  swaymsg move workspace 6 output HDMI-A-2
+  swaymsg move workspace 7 output HDMI-A-2
+  swaymsg move workspace 8 output HDMI-A-2
+  swaymsg move workspace 9 output HDMI-A-2
+  swaymsg move workspace 10 output HDMI-A-2
 fi
