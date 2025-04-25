b88141285e3330a0c55413fe540dc6439b5d77b8
Takuya-Mukai <takuya.mukai@gmail.com>
2025-04-25 16:47:09 +0900

    2025/04/25 dotfiles modification of yay

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,4 +1,8 @@
-./scripts/pacman.sh
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+yay -S --needed - < ./files/installs.txt
 ./scripts/setfile.sh
 ./scripts/security.sh
 ./scripts/theme.sh
