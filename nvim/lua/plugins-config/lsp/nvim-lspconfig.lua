3984558d2570ffac01e70bd43988ae06b92e8a21
Takuya-Mukai <takuya.mukai@gmail.com>
2025-06-05 02:53:20 +0900

    2025/06/05 lsp warning

--- nvim/lua/plugins-config/lsp/nvim-lspconfig.lua
+++ nvim/lua/plugins-config/lsp/nvim-lspconfig.lua
@@ -46,7 +46,7 @@ local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- すべてのLSP共通設定
-vim.lsp.set_log_level("info")
+vim.lsp.set_log_level("info") -- ログレベルを設定
 
 -- lua_ls に特化した設定
 lspconfig.lua_ls.setup({
@@ -69,3 +69,50 @@ lspconfig.lua_ls.setup({
     },
   },
 })
+vim.diagnostic.config({
+  -- 仮想テキスト (コードの行末に表示されるメッセージ)
+  virtual_text = {
+    enable = true, -- true: 有効, false: 無効
+    -- prefix = '●', -- メッセージの前に表示されるシンボル（Nerd Fontsを使っているならアイコンなども指定可能）
+    -- severity_limit = "Information", -- Information以下の診断メッセージも表示する場合 (デフォルトはWarning以上)
+    -- format = function(diagnostic) return diagnostic.message end, -- メッセージのフォーマット
+  },
+  -- サイン (行番号の左側に表示されるアイコン)
+  signs = {
+    enable = true, -- true: 有効, false: 無効
+    -- severity_limit = "Information",
+  },
+  -- アンダーライン (問題のあるコード部分の下線)
+  underline = true, -- true: 有効, false: 無効
+  -- フローティングウィンドウ (カーソルを合わせたときにポップアップ表示)
+  float = {
+    true, --: デフォルトの挙動でポップアップ表示 (カーソルホールド時など)
+    -- false: ポップアップを自動表示しない
+    -- もしくはテーブルで詳細設定
+    -- source = "always", -- 常にソースを表示
+    -- header = "", -- ヘッダーに表示するテキスト
+    border = "single", -- ボーダースタイル (none, single, double, rounded)
+  },
+  -- クイックフィックスリストに表示
+  -- loclist = {
+  --   open = false, -- 診断があるときにloclistを自動で開くか
+  --   severity = { min = vim.diagnostic.severity.WARN }, -- どの深刻度以上をloclistに表示するか
+  -- },
+  -- インサートモード中に診断を更新するか (trueだと重くなる可能性あり)
+  update_in_insert = false,
+  -- 診断の深刻度によるソート順 (trueだとErrorが一番上に来るなど)
+  severity_sort = true,
+})
+
+-- 各Severityのアイコンを設定する例 (Nerd Fontsを使用している場合)
+local signs = {
+  Error = "", -- ファットなX
+  Warn = "",   -- 感嘆符
+  Info = "",   -- 情報アイコン
+  Hint = "󰌵"    -- 電球
+}
+
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type -- DiagnosticSignError, DiagnosticSignWarnなど
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
