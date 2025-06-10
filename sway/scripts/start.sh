f0fe523e79be0ccd004fbf5cb4dbf68ecde70361
Takuya-Mukai <takuya.mukai@gmail.com>
2025-06-10 17:31:18 +0900

    2025/06/10

---
+++ sway/scripts/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# ホスト名を取得
+hostname=$(cat /etc/hostname)
+
+# ホスト名が archdesk の場合
+if [ "$hostname" = "archdesk" ]; then
+  swaymsg "workspace 1; exec gtk-launch chrome-bbdeiblfgdokhlblpgeaokenkfknecgl-Default"
+  swaymsg "workspace 7; exec zen-browser"
+  swaymsg "workspace 5; exec kitty"
+  swaymsg "workspace 10; exec jellyfinmediaplayer"
+  swaymsg "workspace 10; exec LINE--LINE--1748362244.746681"
+fi
+if [ "$hostname" = "archmini" ]; then
+  swaymsg "workspace 1; exec kitty"
+fi
---
+++ waybar/shutdown.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -p "Shut down now? (y/n): " answer
+
+case "$answer" in
+    [Yy]* )
+        echo "Shutting down..."
+        shutdown now
+        ;;
+    [Nn]* )
+        echo "Canceled."
+        sleep 0.7
+        ;;
+    * )
+        echo "Invalid input. Use y or n."
+        sleep 0.7
+        ;;
+esac
