dfd0ed8fcf80bb8f02a30412f3414bb9a56ccbba
Takuya-Mukai <takuya.mukai@gmail.com>
2025-06-16 02:58:20 +0900

    2025/06/16

--- sway/scripts/start.sh
+++ sway/scripts/start.sh
@@ -5,11 +5,11 @@ hostname=$(cat /etc/hostname)
 
 # ホスト名が archdesk の場合
 if [ "$hostname" = "archdesk" ]; then
-  swaymsg "workspace 10; exec gtk-launch LINE--LINE--1748362244.746681"
   swaymsg "workspace 10; exec jellyfinmediaplayer"
-  swaymsg "workspace 1; exec gtk-launch chrome-bbdeiblfgdokhlblpgeaokenkfknecgl-Default"
-  swaymsg "workspace 5; exec foot"
-  swaymsg "workspace 7; exec zen-browser"
+  swaymsg "workspace 10; exec gtk-launch chrome-bbdeiblfgdokhlblpgeaokenkfknecgl-Default"
+  swaymsg "workspace 4; exec zen-browser"
+  swaymsg "workspace 1; exec foot"
+  swaymsg "workspace 10; exec gtk-launch LINE--LINE--1748362244.746681"
 fi
 if [ "$hostname" = "archmini" ]; then
   swaymsg "workspace 1; exec foot"
